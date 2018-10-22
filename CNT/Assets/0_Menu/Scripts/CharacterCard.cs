5e293015d5bf882d7939c09f8eda5bd146ceda0d
JuanPabloSenia <juans.200@hotmail.com>
2018-10-22 16:58:58 -0300

    Revert "Primer Upload del proyecto"
    This reverts commit c84a42394a7d0ebd86013edeb0811741906e18f7.

--- CNT/Assets/0_Menu/Scripts/CharacterCard.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Character", menuName = "New Character")]
-public class CharacterCard : ScriptableObject{
-    public string characterName;
-    public Sprite artWork;
-    public string description;
-}
